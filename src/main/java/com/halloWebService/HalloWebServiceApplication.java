7dc1900be93e9c47ed904f269dbb6cbf6a50dc63
Lwazi <lwazigxogxa@gmail.com>
2024-10-28 13:24:03 +0200

    first commit

---
+++ src/main/java/com/halloWebService/HalloWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.halloWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalloWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalloWebServiceApplication.class, args);
+	}
+
+}
